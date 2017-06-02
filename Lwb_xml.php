9d9fabb1f594944bfc4664e537c107515196fa13
wen1750686723 <wen1750686723@qq.com>
2017-06-02 16:35:09 +0800

    xml

--- Lwb_xml.php
+++ Lwb_xml.php
@@ -11,34 +11,103 @@
  */
 class Lwb_xml
 {
+    public $prepath=TMPL_PATH."statics/evaluate/xml/1/";
     /**
     * 解析xml
     *
     * @access      public
-    * @param       string      $xmlpath        xml所在地址
-    * @return      array.      解析好的xml      
+    * @param       string      $path        xml所在地址    
     */
-	public function parsexml($xmlpath){
-		$xml_parse=simplexml_load_file($xmlpath,'SimpleXMLElement', LIBXML_NOCDATA);
-		return (array)$xml_parse;
-	}
+    public function __construct($path="") {
+        if ($path=="") {
+            # code...
+        }else{
+            $this->prepath=$path;
+        }
+    }
+    public function parsexml($xmlpath){
+        $xmlpath=$this->prepath.$xmlpath;
+        $xml_parse=simplexml_load_file($xmlpath,'SimpleXMLElement', LIBXML_NOCDATA);
+        return (array)$xml_parse;
+    }
     /**
-    * 解析xml
+    * 获取answer
     *
     * @access      public
-    * @param       string      $xmlpath        xml所在地址
     * @return      array.      解析好的array      
     */
-    public function xml_to_array($xmlpath){
-        $mainxml=$this->parsexml($xmlpath);
+    public function getanswer(){
+        $mainxml=$this->parsexml("Answer.xml");
         $mainarray=array();
         foreach ($mainxml["Answer"] as $key => $value) {
             $value=(array)$value;
-            $mainarray[$value["@attributes"]["id"]]["type"]=$value["Type"];
-            $mainarray[$value["@attributes"]["id"]]["Color"]=$value["Color"];
-            $mainarray[$value["@attributes"]["id"]]["TypeDescribe"]=(array)$value["TypeDescribe"];
-            $mainarray[$value["@attributes"]["id"]]["DescribeAll"]=$value["DescribeAll"];
+            $mainarray[$value["@attributes"]["id"]]=$value;
+            
         }
-        var_dump($mainarray);
+        return $mainarray;
+    }
+    /**
+    * 获取interst描述
+    *
+    * @access      public
+    * @return      array.      解析好的array      
+    */
+    public function getinterst(){
+        $mainxml=$this->parsexml("IntrestDescription.xml");
+        $mainarray=array();
+        foreach ($mainxml["IntrestDescription"] as $key => $value) {
+            $value=(array)$value;
+            $mainarray[$value["@attributes"]["id"]]=$value;
+            
+        }
+        return $mainarray;
+    }
+    /**
+    * 获取major描述
+    *
+    * @access      public
+    * @return      array.      解析好的array      
+    */
+    public function getmajor(){
+        $mainxml=$this->parsexml("MajorDescription.xml");
+        $mainarray=array();
+        foreach ($mainxml["Subject"] as $key => $value) {
+            $value=(array)$value;
+            $mainarray[$value["@attributes"]["id"]]=$value;
+            
+        }
+        return $mainarray;
+    }
+    /**
+    * 获取extendspecial描述
+    *
+    * @access      public
+    * @return      array.      解析好的array      
+    */
+    public function getextendspecial(){
+        $mainxml=$this->parsexml("ExtendSpecial.xml");
+        $mainarray=array();
+        foreach ($mainxml["Special"] as $key => $value) {
+            $value=(array)$value;
+            $mainarray[$value["@attributes"]["Id"]]=$value;
+            
+        }
+        return $mainarray;
+    }
+    /**
+    * 获取overseasability描述
+    *
+    * @access      public
+    * @return      array.      解析好的array      
+    */
+    public function getoverseasability(){
+        $mainxml=$this->parsexml("OverseasaAdaptability.xml");
+        $mainarray=array();
+        foreach ($mainxml["Oversea"] as $key => $value) {
+            $value=(array)$value;
+            $mainarray[$value["@attributes"]["id"]]=$value;
+            
+        }
+        return $mainarray;
     }
 }
