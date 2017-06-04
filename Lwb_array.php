0af9ed631f8ac50da0c5f202d7d27c8aa068c626
wen1750686723 <wen1750686723@qq.com>
2017-06-04 23:38:45 +0800

    digui

--- Lwb_array.php
+++ Lwb_array.php
@@ -10,5 +10,27 @@
  * $Id: Lwb_array.php 17155 2017-02-06 06:29:05Z $
  */
 class Lwb_array{
-	
+     /**
+     * 递归获取父类型变量，可以匹配数组或者查库
+     *
+     * @access      public
+     * @param       string      $cid            自身字符串
+     * @param       string      $pid            截止的pid
+     * @return      array      返回的父id数组
+     */
+	function getfarray($cid,$pid=0){
+		// static $array;
+		// $array=array();
+	 //    $criteria = new CDbCriteria;
+		// $criteria->condition = "cid=".$cid;
+		// $cgs = Category::model()->find($criteria);
+		// if ($cgs->fid==$pid or $cgs->fid==0) {
+		// 	$array[]=array("name"=>$cgs->name,"cid"=>$cgs->cid);
+		// }
+		// else{
+		// 	$array[]=array("name"=>$cgs->name,"cid"=>$cgs->cid);
+		// 	getfarray($cgs->fid,$pid);
+		// }
+		// return array_reverse($array);
+	}
 }
